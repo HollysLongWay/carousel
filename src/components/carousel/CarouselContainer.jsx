112f9fc6aba6346a75f763fa6bae8459d007ee09
parksaneon <sangeon1500@daum.net>
2021-12-06 19:41:59 +0900

    feat : play, pasue slide

--- src/components/carousel/CarouselContainer.jsx
+++ src/components/carousel/CarouselContainer.jsx
@@ -15,21 +15,28 @@ export default function CarouselContainer() {
   ];
 
   const [slide, setSlide] = useState(0);
+  const [play, setPlay] = useState(true);
 
   const clickPrev = () => {
-    setSlide(slide - 1 < 0 ? slide - 1 : carouselList.length - 1);
+    setSlide(slide - 1 < 0 ? carouselList.length - 1 : slide - 1);
   };
 
   const clickNext = () => {
     setSlide(slide + 1 > carouselList.length - 1 ? 0 : slide + 1);
   };
 
+  const clickPlay = () => {
+    setPlay(!play);
+  };
+
   useEffect(() => {
-    let timer = setTimeout(() => {
-      setSlide(slide + 1 > carouselList.length - 1 ? 0 : slide + 1);
-    }, 3000);
+    let timer =
+      play &&
+      setTimeout(() => {
+        setSlide(slide + 1 > carouselList.length - 1 ? 0 : slide + 1);
+      }, 3000);
     return () => clearTimeout(timer);
-  }, [slide]);
+  }, [slide, play, carouselList.length]);
 
   return (
     <div className="carousel--container">
@@ -43,13 +50,23 @@ export default function CarouselContainer() {
         ))}
       </ul>
       <div className="carouselBtn-container">
-        <button className="btn--prev" onClick={clickPrev}></button>
+        <button className="btn--prev" onClick={clickPrev} aria-label="이전 슬라이드 보기"></button>
         <ul className="carousel--indicatorList">
-          {carouselList.map((carousel, index) => (
-            <li className={`${index === slide ? 'on' : ''} carousel--indicator`}></li>
+          {carouselList.map((_, index) => (
+            <li
+              className={`${index === slide ? 'on' : ''} carousel--indicator`}
+              aria-label={`${index + 1}번째 슬라이드 보기`}
+              title={`${index + 1}번째 슬라이드 보기`}
+            ></li>
           ))}
         </ul>
-        <button className="btn--next" onClick={clickNext}></button>
+        <button className="btn--next" onClick={clickNext} aria-label="다음 슬라이드 보기"></button>
+        <button
+          className={`${!play ? '' : 'pause'} btn--play`}
+          onClick={clickPlay}
+          aria-label={`슬라이드 ${!play ? '재생' : '정지'} 하기`}
+          title={`슬라이드  ${!play ? '재생' : '정지'} 하기`}
+        ></button>
       </div>
     </div>
   );
