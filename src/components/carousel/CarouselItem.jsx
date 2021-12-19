5f54e7b9b05f50d773ca09ae9199082b9202c974
parksaneon <sangeon1500@daum.net>
2021-12-19 22:23:08 +0900

    feat : edit image extension

--- src/components/carousel/CarouselItem.jsx
+++ src/components/carousel/CarouselItem.jsx
@@ -5,7 +5,7 @@ export default function CarouselItem({ index, slide, src, name }) {
     <li className={`${index === slide ? "show" : ""} carousel--list__item`} key={src}>
       <a href="#" title={`${name} 바로가기`} aria-label={`${name} 바로가기`}>
         <picture>
-          <source srcSet={`${src}.webop`} />
+          <source srcSet={`${src}.webp`} />
           <source srcSet={`${src}.png`} />
           <img src={`${src}.jpg`} alt={name} />
         </picture>
