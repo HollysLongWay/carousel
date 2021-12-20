3e43c614e998d56ad46d5153d4a96a3d10c30539
parksaneon <sangeon1500@daum.net>
2021-12-20 18:45:16 +0900

    feat : add desc in cafe menu state object

--- src/components/carousel/CarouselContainer.jsx
+++ src/components/carousel/CarouselContainer.jsx
@@ -4,13 +4,47 @@ import CarouselList from "./CarouselList";
 
 export default function CarouselContainer() {
   const carouselList = [
-    { name: "GLOW LIGHT", src: "./carousel/carousel1" },
-    { name: "MY HOLIDAY MINT", src: "./carousel/carousel2" },
-    { name: "SWEET LEMON", src: "./carousel/carousel3" },
-    { name: "DEEP RED", src: "./carousel/carousel4" },
-    { name: "OAT LATTE", src: "./carousel/carousel5" },
-    { name: "디저트에 커피를 더하다", src: "./carousel/carousel6" },
-    { name: "디저트 페어링", src: "./carousel/carousel7" }
+    {
+      name: "GLOW LIGHT",
+      src: "./carousel/carousel1",
+      desc: { h2: "일상의 순간을 다채롭게 GLOW LIGHT", p: "15000원 이상 구매시 29000원 2021년 11월 26일 금요일 출시" }
+    },
+    {
+      name: "MY HOLIDAY MINT",
+      src: "./carousel/carousel2",
+      desc: { h2: "향긋한 민트가 퍼지다 MY HOLIDAY MINT", p: "민트 초코칩 케이크" }
+    },
+    {
+      name: "SWEET LEMON",
+      src: "./carousel/carousel3",
+      desc: { h2: "제주의 따스함이 느껴지는 SWEET LEMON", p: "제주 레몬 사과차" }
+    },
+    {
+      name: "DEEP RED",
+      src: "./carousel/carousel4",
+
+      desc: { h2: "겨울을 빨갛게 물들이는 DEEP RED", p: "타트 체리 스무디, 레드벨벳 뚱카롱" }
+    },
+    {
+      name: "OAT LATTE",
+      src: "./carousel/carousel5",
+
+      desc: { h2: "마음을 따뜻하게 채워주는 OAT LATTE", p: "우리 곡물 라떼, 바닐라 오트 라떼" }
+    },
+    {
+      name: "디저트에 커피를 더하다",
+      src: "./carousel/carousel6",
+
+      desc: {
+        h2: "디저트에 커피를 더하다 오늘을 즐기다",
+        p: "카페 아메리카노, 촉촉한 당근케이크, 더블 베리 바스크 치즈 케이크"
+      }
+    },
+    {
+      name: "디저트 페어링",
+      src: "./carousel/carousel7",
+      desc: { h2: "디저트 페어링", p: "카페 아메리카노, 카페 라떼, 진한 마스카포네 티라미스" }
+    }
   ];
 
   const [slide, setSlide] = useState(0);
