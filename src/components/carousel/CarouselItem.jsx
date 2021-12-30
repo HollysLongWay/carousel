7fc2b945341b18e519b1fec599f810a2cdea3699
parksaneon <sangeon1500@daum.net>
2021-12-30 11:05:31 +0900

    feat : add sr-only text into carousel items

--- src/components/carousel/CarouselItem.jsx
+++ src/components/carousel/CarouselItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function CarouselItem({ index, slide, src, name }) {
+export default function CarouselItem({ slide, name, src, desc, index }) {
   return (
     <li className={`${index === slide ? "show" : ""} carousel--list__item`} key={src}>
       <a href="#" title={`${name} 바로가기`} aria-label={`${name} 바로가기`}>
@@ -10,6 +10,12 @@ export default function CarouselItem({ index, slide, src, name }) {
           <img src={`${src}.jpg`} alt={name} />
         </picture>
       </a>
+      {desc && (
+        <div className="sr-only">
+          <h2>{desc.h2}</h2>
+          <p>{desc.p}</p>
+        </div>
+      )}
     </li>
   );
 }
--- src/components/carousel/CarouselList.jsx
+++ src/components/carousel/CarouselList.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import CarouselItem, { MemoizedCarouselItem } from "./CarouselItem";
+import { MemoizedCarouselItem } from "./CarouselItem";
 
 export default function CarouselList({ carouselList, slide }) {
   return (
     <ul className="carousel--list">
-      {carouselList.map(({ name, src }, index) => (
-        <MemoizedCarouselItem key={src} role="carouselItem" index={index} slide={slide} src={src} name={name} />
+      {carouselList.map(({ name, src, desc }, index) => (
+        <MemoizedCarouselItem
+          key={src}
+          role="carouselItem"
+          slide={slide}
+          name={name}
+          src={src}
+          desc={desc}
+          index={index}
+        />
       ))}
     </ul>
   );
