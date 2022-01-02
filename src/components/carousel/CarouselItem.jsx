15e0d1665b39f1eb414d5d82b8e76202e7090449
parksaneon <sangeon1500@daum.net>
2022-01-02 18:10:00 +0900

    feat : move carousel description

--- src/components/carousel/CarouselItem.jsx
+++ src/components/carousel/CarouselItem.jsx
@@ -5,19 +5,19 @@ export default function CarouselItem({ slide, name, src, desc, index }) {
 
   return (
     <li className={`${index === slide ? "show" : ""} carousel--list__item`} key={src}>
-      <a href="#" title={`${name} 바로가기`} aria-label={`${name} 바로가기`}>
-        <picture>
-          <source srcSet={`${src}.webp`} />
-          <source srcSet={`${src}.png`} />
-          <img src={`${src}.jpg`} alt={name} />
-        </picture>
-      </a>
       {desc && (
         <div className="sr-only">
           <h2>{title}</h2>
           <p>{description}</p>
         </div>
       )}
+      <a href="#" title={`${name} 바로가기`}>
+        <picture>
+          <source srcSet={`${src}.webp`} />
+          <source srcSet={`${src}.png`} />
+          <img src={`${src}.jpg`} alt={name} />
+        </picture>
+      </a>
     </li>
   );
 }
