0c75d451fba8795ae5cde3ab7f41a64075a8c45e
parksaneon <sangeon1500@daum.net>
2022-03-04 14:43:42 +0900

    feat : add Button.module.css for style of Button Component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Carousel2 from "./components/carousel2/Carousel2";
 import TodoList from "./components/todos/TodoList";
 import "./scss/App.scss";
 import ShoppingMall from "./components/shoppingMall/ShoppingMall";
+import Button from "components/buttons/Button";
 
 function App() {
   return (
@@ -13,7 +14,12 @@ function App() {
         {/* <CarouselContainer /> */}
         {/* <TodoList /> */}
         {/* <Carousel2 /> */}
-        <ShoppingMall />
+        {/* <ShoppingMall /> */}
+        <Button state="upload" />
+        <Button state="uploading" />
+        <Button state="complete" />
+        <Button state="fail" />
+        <Button state="disable" />
       </main>
     </HashRouter>
   );
--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,13 +1,22 @@
+import styles from "./Button.module.css";
 import { buttonProp } from "./Button.type";
 
 const Button = ({ state }: buttonProp) => {
-  const contentObj = { upload: "업로드", uploading: "업로드 중", complete: "성공", fail: "실패", disable: "실패" };
+  const contentObj = {
+    upload: "업로드",
+    uploading: "업로드 중",
+    complete: "성공",
+    fail: "실패",
+    disable: "업로드"
+  };
   const content = contentObj[state];
 
   return (
-    <button type="button" disabled={state === "fail" ? true : false}>
+    <button type="button" disabled={state === "disable" ? true : false} className={styles.button}>
       {content}
       <img src={`${process.env.PUBLIC_URL}/img/buttons/${state}.png`} alt={content} />
     </button>
   );
 };
+
+export default Button;
