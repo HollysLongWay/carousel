b88119d605ff3dfc4bcc16f221c6396d8e3e3806
parksaneon <sangeon1500@daum.net>
2022-03-10 23:21:03 +0900

    feat : Price Component style

--- src/components/purchase/price/Price.tsx
+++ src/components/purchase/price/Price.tsx
@@ -4,9 +4,15 @@ import PriceProps from "./Price.type";
 const Price = ({ quantity, price }: PriceProps) => {
   const totalPrice: Number = +price * +quantity;
   return (
-    <div>
-      <span>총 수량 {quantity}개</span>
-      <span>{totalPrice.toLocaleString()}원</span>
+    <div className={styles.Price}>
+      <p>
+        총 상품 금액
+        <img src={process.env.PUBLIC_URL + "/img/purchase/HelfButton.png"} alt="" />
+      </p>
+      <div>
+        <span className={styles.totalQuantity}>총 수량 {quantity}개</span>
+        <span className={styles.totalPrice}>{totalPrice.toLocaleString()}원</span>
+      </div>
     </div>
   );
 };
